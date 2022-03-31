acfbaa2d21158faade0e869fbe1a380a0c1a1ed1
Irina Karpova <iirakarpova@gmail.com>
2022-03-31 21:30:18 +0200

    Add psycopg2

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/irinakarpova/Programming/Web Course/Python/maps/env"
+set -gx VIRTUAL_ENV "/Users/irinakarpova/Programming/Web Course/lolytest_project/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
